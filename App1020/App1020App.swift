5131a9e41014433821b6f855138408ea08d458cb
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-11-09 16:23:11 +0300

    Initial Commit

---
+++ App1020/App1020App.swift
@@ -0,0 +1,17 @@
+//
+//  App1020App.swift
+//  App1020
+//
+//  Created by IGOR on 09/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct App1020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
